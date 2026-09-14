73b33c6cebc30ac9d32447abd4bc708fa9858fef
Felipe Lustosa <felipelustosaa6@gmail.com>
2026-09-14 09:42:42 -0300

    FEAT: remove rented movies from available movies list

--- lib/src/features/movies/ui/widgets/tab_bar_content/movies_content.dart
+++ lib/src/features/movies/ui/widgets/tab_bar_content/movies_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_flca2/src/core/di/injection.dart';
 import 'package:project_flca2/src/features/movies/controller/available_movies_controller.dart';
+import 'package:project_flca2/src/features/movies/controller/rental_movies_controller.dart';
 import 'package:project_flca2/src/features/movies/ui/widgets/grid_view.dart';
 import 'package:signals/signals_hooks.dart';
 
@@ -13,18 +14,28 @@ class MoviesContent extends StatefulWidget {
 
 class _MoviesContentState extends State<MoviesContent> {
   final _movieController = getIt<AvailableMoviesController>();
+  final _rentalMovieController = getIt<RentalMoviesController>();
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: SignalBuilder(
-        builder: (context) => GridBuilder(
-          onTap: (movie) {
-            _movieController.selectMovie(movie, isRental: false);
-          },
-          movies: _movieController.moviesList,
-        ),
+        builder: (context) {
+          final movies = _movieController.moviesList;
+          final rentalMovies = _rentalMovieController.rentalMoviesList;
+
+          final rentedMovieIds = rentalMovies.map((m) => m.id).toSet();
+          final filteredMovies = movies.where((movie) => !rentedMovieIds.contains(movie.id)).toList();
+
+          return GridBuilder(
+            onTap: (movie) {
+              _movieController.selectMovie(movie, isRental: false);
+            },
+            movies: filteredMovies,
+          );
+        },
       ),
     );
   }
 }
+
